73781b7cbdb1b32d9cae5e0f3dcdc428bdd1f2ed
Yash Nathani <y_nathan@live.concordia.ca>
2024-09-19 22:52:47 -0400

    Added basic frontend

---
+++ src/pages/Home.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+  
+        <div className="container">
+            <h1>Welcome to Peer Assessment site</h1>
+            <p>Your feedback will remain anonymous, and protecting your data is our top priority.</p>
+            <div>
+                <h2>How it works</h2>
+                <p>1. You will be asked to provide feedback on your peers' work.</p>
+                <p>2. You will be able to see the feedback you receive from your peers.</p>
+                <p>3. Your feedback will be used to improve your work and the work of your peers.</p>
+            </div>
+        </div>
+
+  )
+}
+
+export default Home
