2bbac9aeef7550118bcea219d18519a63733d635
FabioKoshy <fabiokoshy@gmail.com>
2024-09-28 22:54:05 -0400

    Updated Home.jsx
    Added a few lines that makes the texts justified and auto resizes within the box as the window changes size

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -16,8 +16,8 @@ const Home = () => {
           </div>
           <div className="col-md-8">
             <div className="card-body">
-              <h5 className="card-title"> Understanding the Importance of Peer Assessment </h5>
-              <p className="card-text">
+              <h5 className="card-title" style={{ fontSize: 'clamp(1.25rem, 2vw, 2.5rem)' }}> Understanding the Importance of Peer Assessment </h5>
+              <p className="card-text" style={{ fontSize: 'clamp(1rem, 1.5vw, 2rem)', textAlign: 'justify' }}>
               Peer assessment plays a crucial role in enhancing the learning experience by encouraging students to evaluate each other's work. It fosters a collaborative environment where constructive feedback is shared, helping individuals identify their strengths and areas for improvement. This process not only builds critical thinking and analytical skills but also nurtures a sense of responsibility and accountability among peers.
               </p>
               {/* <p className="card-text">
@@ -32,8 +32,8 @@ const Home = () => {
         <div className="row g-10">
           <div className="col-md-8 order-md-1">
             <div className="card-body">
-              <h5 className="card-title">How it works</h5>
-              <p className="card-text">
+              <h5 className="card-title" style={{ fontSize: 'clamp(1.25rem, 2vw, 2.5rem)' }}>How it works</h5>
+              <p className="card-text" style={{ fontSize: 'clamp(1rem, 1.5vw, 2rem)', textAlign: 'justify'}}>
               Our web application provides an easy-to-use platform for conducting peer assessments. You can submit your work, review the submissions of your peers, and provide feedback using predefined criteria. This structured approach ensures that feedback is fair, constructive, and valuable, ultimately helping everyone to improve and grow together.
               </p>
               {/* <p className="card-text">
