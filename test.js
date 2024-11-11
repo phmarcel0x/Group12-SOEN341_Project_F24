fbe86864c08615fe577df545cfa8965cd3153b67
Yash Nathani <y_nathan@live.concordia.ca>
2024-11-10 19:23:17 -0500

    Test caseh

--- test.js
+++ test.js
@@ -1,4 +1,12 @@
-test('adds 1 + 2 to equal 3', () => {
-    expect(1 + 2).toBe(3);
-  });
+// test('adds 1 + 2 to equal 3', () => {
+//     expect(1 + 2).toBe(3);
+//   });
   
+import { render, screen } from '@testing-library/react';
+import { Header } from './Header';
+
+test('Header renders correctly', () => {
+  render(<Header />);
+  const textElement = screen.getByText('Hello');
+  expect(textElement).toBeInTheDocument();
+});
