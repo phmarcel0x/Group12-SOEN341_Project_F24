eaf4228de6374bed685b3bda5fe9ebaf81830b5b
Yash Nathani <y_nathan@live.concordia.ca>
2024-11-10 19:25:25 -0500

    Test case

--- test.js
+++ test.js
@@ -1,12 +1,7 @@
-// test('adds 1 + 2 to equal 3', () => {
-//     expect(1 + 2).toBe(3);
-//   });
+test('adds 1 + 2 to equal 3', () => {
+    expect(1 + 2).toBe(3);
+  });
   
-import { render, screen } from '@testing-library/react';
-import { Header } from './Header';
-
-test('Header renders correctly', () => {
-  render(<Header />);
-  const textElement = screen.getByText('Hello');
-  expect(textElement).toBeInTheDocument();
-});
+test1('adds 2 + 2 to equal 4', () => {
+    expect(1 + 2).toBe(4);
+  });
