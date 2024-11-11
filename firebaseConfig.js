874f15de83f5d3257ea85ff8ea33491882736aac
Yash Nathani <y_nathan@live.concordia.ca>
2024-11-10 19:01:37 -0500

    Testing

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -11,14 +11,15 @@ import { getDatabase } from "firebase/database";  // For Realtime Database (if y
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 
 
-// const firebaseConfig = {
-//   apiKey: "YOUR_API_KEY",
-//   authDomain: "YOUR_AUTH_DOMAIN",
-//   projectId: "YOUR_PROJECT_ID",
-//   storageBucket: "YOUR_STORAGE_BUCKET",
-//   messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-//   appId: "YOUR_APP_ID",
-// };
+const firebaseConfig = {
+  apiKey: "AIzaSyC-GgVnoeRB7KSgRIEBTpyNrtCuUKjk4TA",
+  authDomain: "soen341-project-group12.firebaseapp.com",
+  projectId: "soen341-project-group12",
+  storageBucket: "soen341-project-group12.appspot.com",
+  messagingSenderId: "551475228011",
+  appId: "1:551475228011:web:d14120a0b7549888d0f02e",
+  measurementId: "G-S286NL2V3Z"
+};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
